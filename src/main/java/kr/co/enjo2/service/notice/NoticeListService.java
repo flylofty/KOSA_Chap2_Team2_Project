65fdc76ee2f0d43a47737e62d5be6e7a1e96ea5e
Youngnam Lee <lyn00227@naver.com>
2022-11-25 10:40:33 +0900

    공지사항 페이징 데이터 처리

--- src/main/java/kr/co/enjo2/service/notice/NoticeListService.java
+++ src/main/java/kr/co/enjo2/service/notice/NoticeListService.java
@@ -21,7 +21,10 @@ public class NoticeListService implements Action {
 
 		try {
 			int page = Integer.parseInt(request.getParameter("page"));
-			List<NoticeDto> noticeArr = new NoticeDao().findAllByPage(page);
+			
+			NoticeDao dao = new NoticeDao();
+			
+			List<NoticeDto> noticeArr = dao.findAllByPage(page);
 
 			JSONArray noticeList = new JSONArray();
 			for (NoticeDto n : noticeArr) {
@@ -36,12 +39,52 @@ public class NoticeListService implements Action {
 				obj.put("count", String.valueOf(1));
 				noticeList.add(obj);
 			}
+			
+			//////////// 페이지 계산 ////////////
+			
+			// 게시글의 총 수
+			int totalCount = dao.getTotalCount();
+			
+			// 총 페이지의 수
+			int numOfTotalPage = (totalCount / 10) + ( (totalCount % 10 == 0) ? 0 : 1);
+			
+			int start = 0;
+			int end = 0;
+			
+			for(int n = 1; ; ++n) {
+				start = 5 * n - 4;
+				end = 5 * n;
+				if (start <= page && page <= end) {
+					break;
+				}
+			}
+			
+			int prev = 1;
+			int next = 1;
+			
+			if (start == 1) {
+				prev = 0;
+			}
+			
+			end = Math.min(end, numOfTotalPage);
+			if (end == numOfTotalPage) {
+				next = 0;
+			}
+			
+			JSONObject pageObj = new JSONObject();
+			pageObj.put("prev", String.valueOf(prev));
+			pageObj.put("next", String.valueOf(next));
+			pageObj.put("start", String.valueOf(start));
+			pageObj.put("end", String.valueOf(end));
+			
+			//////////// 페이지 계산 끝 /////////////
 
 			response.setCharacterEncoding("UTF-8");
 			response.setContentType("application/json;charset=UTF-8");
 			PrintWriter out = response.getWriter();
 			JSONObject obj = new JSONObject();
 			obj.put("noticeList", noticeList);
+			obj.put("pageInfo", pageObj);
 			String result = obj.toJSONString();
 			out.print(result);
 			response.setStatus(200);
